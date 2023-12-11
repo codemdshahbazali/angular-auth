a474ca6747900d40473ef97ac5b7d8c0842ba65e
codemdshahbazali <66906468+codemdshahbazali@users.noreply.github.com>
2023-12-11 10:07:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'latest';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
+
+  onNavigate(feature: any) {
+    this.loadedFeature = feature;
+  }
 }
